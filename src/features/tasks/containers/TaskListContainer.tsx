2d6fa6d365f0e5ed80b94f8c0511b5c73e7f3448
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-09-06 17:21:34 +0900

    タスク一覧ページ作成

--- src/features/tasks/containers/TaskListContainer.tsx
+++ src/features/tasks/containers/TaskListContainer.tsx
@@ -1,16 +1,35 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getTasks } from "../hooks/getTasks";
+import { Task } from "../../../types/task";
+import TaskCard from "../components/TaskCard";
+import styles from "../styles/TaskListContainer.module.css";
 
 export default function TaskListContainer() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const fetchedTasks = await getTasks();
+      setTasks(fetchedTasks);
+    };
+    fetchTasks();
+  }, []);
+
   return (
-    <div>
+    <div className={styles.container}>
       <h1>タスク一覧</h1>
-      <div>
-        <Link>
-          <h3>ログイン機能を実装する</h3>
-        </Link>
-        <p>ステータス : 未完了</p>
-        <p>優先度 : 高</p>
-        <p>期限日 : 2025-02-10</p>
+      <div className={styles.tableContainer}>
+        <div className={styles.header}>
+          <div>タスク名</div>
+          <div>ステータス</div>
+          <div>優先度</div>
+          <div>期限日</div>
+        </div>
+        <div className={styles.taskList}>
+          {tasks.map((task) => (
+            <TaskCard key={task.id} task={task} />
+          ))}
+        </div>
       </div>
     </div>
   );
