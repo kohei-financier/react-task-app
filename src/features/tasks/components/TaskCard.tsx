ba865cddcf978d334126014285010823bde08bfa
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-09-09 19:30:39 +0900

    タスク詳細ページ作成まで

--- src/features/tasks/components/TaskCard.tsx
+++ src/features/tasks/components/TaskCard.tsx
@@ -1,31 +1,14 @@
 import { Link } from "react-router-dom";
-import { Priority, Task } from "../../../types/task";
 import styles from "../styles/taskCard.module.css";
+import { getPriorityClass } from "../utils/priority";
+import { getStatusLabel } from "../utils/status-label";
+import { Task } from "../../../types/task";
 
 type Props = {
   task: Task;
 };
 
 export default function TaskCard({ task }: Props) {
-  // 優先度に応じたクラス名を返す関数
-  const getPriorityClass = (priority: Priority): string => {
-    switch (priority.toLowerCase()) {
-      case "high":
-        return styles.highPriority
-      case "medium":
-        return styles.mediumPriority
-      case "low":
-        return styles.lowPriority
-      default:
-        return styles.defaultPriority
-    }
-  }
-
-  // ステータスに応じた表示ラベルを返す関数
-  const getStatusLabel = (status: boolean): string => {
-    return status ? "完了" : "未完了";
-  };
-
   return (
     <div className={styles.taskCard}>
       <Link to={`/tasks/${task.id}`} className={styles.detailLink}>
@@ -34,7 +17,7 @@ export default function TaskCard({ task }: Props) {
       <p className={`${styles.badge} ${task.status ? styles.completed : styles.incomplete}`}>
         {getStatusLabel(task.status)}
       </p>
-      <p className={`${styles.badge} ${getPriorityClass(task.priority)}`}>{task.priority}</p>
+      <p className={`${styles.badge} ${getPriorityClass(task.priority, styles)}`}>{task.priority}</p>
       <p className={styles.taskDueDate}>{task.dueDate}</p>
     </div>
   );
